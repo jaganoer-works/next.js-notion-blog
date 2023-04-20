0c742644a36bdf7e6b33fd175374cca79a96d255
jaganoer-works <jaganoer@gmail.com>
2023-04-21 07:59:48 +0900

    ハイドレーションエラーを修正

--- components/post/single-post.tsx
+++ components/post/single-post.tsx
@@ -13,10 +13,12 @@ const SinglePost = (props: Props) => {
   const { title, description, date, tags, slug } = props;
 
   return (
-    <Link href={`posts/${slug}`}>
+    <>
       <section className="lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
         <div className="flex items-center gap-3">
-          <h2 className="text-gray-100 text-2xl font-medium mb-2">{title}</h2>
+          <h2 className="text-gray-100 text-2xl font-medium mb-2">
+            <Link href={`posts/${slug}`}>{title}</Link>
+          </h2>
           <div className="text-gray-100">{date}</div>
           {tags.map((tag) => (
             <>
@@ -28,7 +30,7 @@ const SinglePost = (props: Props) => {
         </div>
         <p className="text-gray-100">{description}</p>
       </section>
-    </Link>
+    </>
   );
 };
 
