543b97cf506eddbf0ccbb8972d608b6d1c3a6c00
jaganoer-works <jaganoer@gmail.com>
2023-04-21 11:35:46 +0900

    詳細記事のデータを取得するNotionAPIを実装、SSG（ISR）ページにgetStaticPathsを追加する

--- lib/notion-api.tsx
+++ lib/notion-api.tsx
@@ -16,7 +16,6 @@ export const getAllPosts = async () => {
 };
 
 const getPageMetaData = (post) => {
-  
   const getTags = (tags) => {
     const allTags = tags.map((tag) => tag.name);
     return allTags;
@@ -29,5 +28,27 @@ const getPageMetaData = (post) => {
     date: post.properties.Date.date.start,
     slug: post.properties.Slug.rich_text[0].plain_text,
     tags: getTags(post.properties.Tags.multi_select),
+  };
+};
+
+export const getSingePost = async (slug) => {
+  const response = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+    filter: {
+      property: "Slug",
+      formula: {
+        string: {
+          equals: slug,
+        },
+      },
+    },
+  });
+
+  const page = response.results[0];
+  const metadata = getPageMetaData(page);
+  console.log(metadata);
+
+  return {
+    metadata,
   };
 };
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,6 +1,31 @@
 import React from "react";
+import { getSingePost } from "../../lib/notion-api";
 
-const Post = () => {
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { slug: "first-post" } },
+      { params: { slug: "second-post" } },
+      { params: { slug: "third-post" } },
+    ],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const post = await getSingePost(params.slug);
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 10,
+  };
+};
+
+const Post = ({ post }) => {
+  console.log(post);
+
   return (
     <section className="container lg:px-2 px-5 h-screen lg:w-2/5 mx-auto mt-20">
       <h2 className="w-full text-2xl font-medium">３回目の投稿です。</h2>
@@ -10,9 +35,7 @@ const Post = () => {
       <p className="text-white bg-sky-900 rounded-xl font-medium mt-2 px-2 inline-block">
         Next.js
       </p>
-      <div className="mt-10">
-        aaaadfaodkjaojdaijdioajdjaoijdoajij
-      </div>
+      <div className="mt-10">aaaadfaodkjaojdaijdioajdjaoijdoajij</div>
     </section>
   );
 };
