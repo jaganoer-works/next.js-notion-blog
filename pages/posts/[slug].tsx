dfb3e5c8fbe44f0c993c736e532e3d2e7b12729e
jaganoer-works <jaganoer@gmail.com>
2023-04-21 11:00:24 +0900

    詳細ページを作成

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <section className="container lg:px-2 px-5 h-screen lg:w-2/5 mx-auto mt-20">
+      <h2 className="w-full text-2xl font-medium">３回目の投稿です。</h2>
+      <div className="border-b-2"></div>
+      <span className="text-gray-500">2023/4/19</span>
+      <br />
+      <p className="text-white bg-sky-900 rounded-xl font-medium mt-2 px-2 inline-block">
+        Next.js
+      </p>
+      <div className="mt-10">
+        aaaadfaodkjaojdaijdioajdjaoijdoajij
+      </div>
+    </section>
+  );
+};
+
+export default Post;
