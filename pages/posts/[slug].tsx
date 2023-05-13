d619b0066fee128833021ce38a9f4c5c2c5788dc
jaganoer-works <jaganoer@gmail.com>
2023-05-13 18:31:57 +0900

    コードのシンタックスハイライトを設定、ダークモードにする、スタイルの調整

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { getAllPosts, getSingePost } from "../../lib/notion-api";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import Link from "next/link";
 
 export const getStaticPaths = async () => {
   const allPost = await getAllPosts();
@@ -25,7 +28,6 @@ export const getStaticProps = async ({ params }) => {
 
 const Post = ({ post }) => {
   console.log(post);
-
   return (
     <section className="container lg:px-2 px-5 h-screen lg:w-2/5 mx-auto mt-20">
       <h2 className="w-full text-2xl font-medium">{post.metadata.title}</h2>
@@ -37,8 +39,32 @@ const Post = ({ post }) => {
           {tag}
         </p>
       ))}
-      <div className="mt-10 font-medium">
-        <ReactMarkdown children={post.markdown.parent} />
+      <div className="markdown mt-10 font-medium">
+        <ReactMarkdown
+          children={post.markdown.parent}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  {...props}
+                  children={String(children).replace(/\n$/, "")}
+                  style={vscDarkPlus}
+                  language={match[1]}
+                  PreTag="div"
+                />
+              ) : (
+                <code {...props} className={className}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+        />
+
+        <Link href="/">
+          <span className="pb-20 block mt-3 text-sky-900">←ホームへ戻る</span>
+        </Link>
       </div>
     </section>
   );
