afaf1e4a9534a02637a92ab561f397e07ec2d7d2
jaganoer-works <jaganoer@gmail.com>
2023-05-16 00:11:59 +0900

    トップページの記事一覧のデザインを変更する

--- components/post/single-post.tsx
+++ components/post/single-post.tsx
@@ -33,21 +33,21 @@ const SinglePost = (props: Props) => {
           <p className="text-gray-100">{description}</p>
         </section>
       ) : (
-        <section className="lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
+        <section className="border-2 border-gray-200 border-opacity-60 lg:w-1/2 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
           <div className="flex items-center gap-3">
-            <h2 className="text-gray-100 text-2xl font-medium mb-2">
+            <h2 className="text-gray-500 text-2xl font-medium mb-2">
               <Link href={`posts/${slug}`}>{title}</Link>
             </h2>
-            <div className="text-gray-100">{date}</div>
-            {tags.map((tag: string, index: number) => (
-              <Link key={index} href={`/posts/tag/${tag}/page/1`}>
-                <span className="text-white bg-gray-500 rounded-xl px-2 pb-1 font-medium">
-                  {tag}
-                </span>
-              </Link>
-            ))}
+            <div className="text-gray-500">{date}</div>
           </div>
-          <p className="text-gray-100">{description}</p>
+          {tags.map((tag: string, index: number) => (
+            <Link key={index} href={`/posts/tag/${tag}/page/1`}>
+              <span className="text-white bg-gray-500 rounded-xl px-2 pb-1 mr-2 font-medium">
+                {tag}
+              </span>
+            </Link>
+          ))}
+          <p className="text-gray-500">{description}</p>
         </section>
       )}
     </>
