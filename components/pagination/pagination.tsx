37a3001a3108a57de0cc4618136f22dff714e3fa
jaganoer-works <jaganoer@gmail.com>
2023-05-20 01:45:24 +0900

    ページネーションのデザインを変更 #12

--- components/pagination/pagination.tsx
+++ components/pagination/pagination.tsx
@@ -16,27 +16,26 @@ const Pagination = (props: Props) => {
     pages.push(i);
   }
 
-  const isCurrentPage = (page: number) => {
-    if (page === currentPage) {
-      return "bg-white border border-gray-500 rounded-lg w-10 h-10 text-gray-500 relative";
-    }
-    return "bg-blue-500 rounded-lg w-10 h-10 text-gray-100 relative hover:bg-white hover:text-gray-500 hover:border hover:border-gray-500";
-  };
+  // const isCurrentPage = (page: number) => {
+  //   if (page === currentPage) {
+  //     return "bg-white border border-gray-500 rounded-lg w-10 h-10 text-gray-500 relative";
+  //   }
+  //   return "bg-blue-500 rounded-lg w-10 h-10 text-gray-100 relative hover:bg-white hover:text-gray-500 hover:border hover:border-gray-500";
+  // };
 
   return (
-    <section className="mb-8 lg:w-1/2 mx-auto rounded-md p-5">
-      <ul className="flex items-center justify-center gap-4">
+    <section className="flex place-content-center mb-5">
+      <div className="btn-group">
         {pages.map((page) => (
-          <li key={page} className={isCurrentPage(page)}>
-            <Link
-              href={getPageLink(tag, page)}
-              className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4"
-            >
-              {page}
-            </Link>
-          </li>
+          <Link href={getPageLink(tag, page)} legacyBehavior>
+            {page === currentPage ? (
+              <a className="btn btn-active">{page}</a>
+            ) : (
+              <a className="btn">{page}</a>
+            )}
+          </Link>
         ))}
-      </ul>
+      </div>
     </section>
   );
 };
