46f6628cc1cd7b9f8ac08903e1daf15a993b015c
jaganoer-works <jaganoer@gmail.com>
2023-05-20 02:19:52 +0900

    ブログ詳細ページへ遷移できないエラーを修正 #12

--- components/post/single-post.tsx
+++ components/post/single-post.tsx
@@ -11,23 +11,21 @@ type Props = {
 
 const SinglePost = (props: Props) => {
   const { title, description, date, tags, slug } = props;
-
   return (
     <div className="card w-full bg-primary text-primary-content">
       <div className="card-body">
         <h2 className="card-title">
-          <Link href={`posts/${slug}`}>{title}</Link>
+          <Link href={`/posts/${slug}`}>{title}</Link>
         </h2>
         <p>{description}</p>
         <div className="text-right mb-2">
           {tags.map((tag: string, index: number) => (
             <Link key={index} href={`/posts/tag/${tag}/page/1`}>
-              <span className="badge badge-secondary p-2 mr-2">
-                {tag}
-              </span>
+              <span className="badge badge-secondary p-2 mr-2">{tag}</span>
             </Link>
           ))}
         </div>
+        <div className="stat-desc text-right">{date}</div>
       </div>
     </div>
   );
