e4616703b6c1d164657593569d0caa80d8b42c4f
jaganoer-works <jaganoer@gmail.com>
2023-05-20 17:31:42 +0900

    サイトのメタ情報を設定するコンポーネントを作成 #9

---
+++ components/meta/meta.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { SITE_META } from "@/constants/constants";
+
+type MetaProps = {
+  pageTitle?: string;
+  pageDesc?: string;
+};
+
+const Meta = ({ pageTitle, pageDesc }: MetaProps) => {
+  const { siteTitle, siteDesc, siteURL, siteType, siteLocale, siteIcon } =
+    SITE_META;
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle;
+  const desc = pageDesc ?? siteDesc;
+  const router = useRouter();
+  const url = `${siteURL}${router.asPath}`;
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={desc} />
+      <link ref="canonical" href={url} />
+      <meta property="og:url" content={url} />
+      <meta property="og:site_name" content={siteTitle} />
+      <meta property="og:type" content={siteType} />
+      <meta property="og:locale" content={siteLocale} />
+      <link rel="icon" href={siteIcon} />
+      <link rel="apple-touch-icon" href={siteIcon} />
+      {/* TODO: OGP画像を追加する */}
+    </Head>
+  );
+};
+
+export default Meta;
