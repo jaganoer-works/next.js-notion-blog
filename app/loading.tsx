5105b4f6016720aa1fb1d28a281e685fc4f239bc
jaganoer-works <jaganoer@gmail.com>
2023-06-26 00:21:05 +0900

    loading.tsxを追加

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center h-screen -mt-10 items-center">
+        <div className="loading loading-dots loading-lg h-32 w-32"></div>
+    </div>
+  );
+}
